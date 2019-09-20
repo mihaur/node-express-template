bc25391af2b603825af258de7e2a11efd76f0ff9
Miha Urbanija <miha.urbanija@comtrade.com>
2019-09-20 13:25:28 +0200

    chore: add JSDoc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import 'dotenv/config'
 
-const greeter = (name = process.env.GREETING) => {
+/**
+ * @function greeter
+ * @description
+ * Generates a custom greeting 'Hello, Name!', where input parameter name is capitalized. If name is not given process.env.GREETING is instead as default value.
+ * @summary Returns a greeting.
+ *
+ * @param {String} [name=process.env.GREETING] - name to append to greeting, name is capitalized
+ * @returns {String} 'Hello, Name!' greeting
+ * @throws {TypeError} string expected, got type
+ *
+ * @example
+ * // Generate default greeting (depending on process.env.GREETING)
+ * const result = greeter()
+ * //=> Hello, World!
+ *
+ * @example
+ * // Greet John
+ * const result = greeter('John')
+ * //=> Hello, John!
+ *
+ * @example
+ * // Greet Jane
+ * const result = greeter('jane')
+ * //=> Hello, Jane!
+ *
+ */
+
+export default function greeter (name = process.env.GREETING) {
   if (typeof name !== 'string') {
     throw new TypeError('string expected, got ' + typeof name)
   }
